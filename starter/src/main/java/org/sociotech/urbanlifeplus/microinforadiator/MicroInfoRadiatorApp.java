d3aa2e210ed8a6a3046c4ba43e9fef6d60e71629
Vitus Lehner <student@vitus-lehner.de>
2017-06-14 12:33:02 +0200

    Added basic Gradle multi-project structure with Gradle Wrappers

---
+++ starter/src/main/java/org/sociotech/urbanlifeplus/microinforadiator/MicroInfoRadiatorApp.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017. Vitus Lehner. UrbanLife+. Universität der Bundeswehr München.
+ */
+
+package org.sociotech.urbanlifeplus.microinforadiator;
+
+public class MicroInfoRadiatorApp {
+
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MicroInfoRadiatorApp().getGreeting());
+    }
+
+}
---
+++ starter/src/test/java/org/sociotech/urbanlifeplus/microinforadiator/MicroInfoRadiatorAppTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2017. Vitus Lehner. UrbanLife+. Universität der Bundeswehr München.
+ */
+
+package org.sociotech.urbanlifeplus.microinforadiator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class MicroInfoRadiatorAppTest {
+    @Test public void testAppHasAGreeting() {
+        MicroInfoRadiatorApp classUnderTest = new MicroInfoRadiatorApp();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
