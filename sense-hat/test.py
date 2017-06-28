2726dfcd829a82ca0b143c62420edbcb60fe36ba
Vitus Lehner <vituslehner@users.noreply.github.com>
2017-06-28 13:33:49 +0200

    Sense hat testing

--- sense-hat/test.py
+++ sense-hat/test.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python
 import sys
+import time
 from sense_hat import SenseHat
 
-# To get good results with the magnetometer you must first calibrate it using
-# the program in RTIMULib/Linux/RTIMULibCal
-# The calibration program will produce the file RTIMULib.ini
-# Copy it into the same folder as your Python code
+X = (255, 0, 0)
+O = (255, 255, 255)
 
-led_loop = [4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8, 0, 1, 2, 3]
+question_mark = [
+    O, O, O, X, X, O, O, O,
+    O, O, X, O, O, X, O, O,
+    O, O, O, O, O, X, O, O,
+    O, O, O, O, X, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, X, O, O, O, O,
+    O, O, O, O, O, O, O, O,
+    O, O, O, X, O, O, O, O
+]
 
 sense = SenseHat()
-sense.set_rotation(0)
-sense.clear()
 
-prev_x = 0
-prev_y = 0
+sense.set_pixels(question_mark)
 
-led_degree_ratio = len(led_loop) / 360.0
+sense.set_pixel(0, 0, 255, 0, 0)
+sense.set_pixel(0, 7, 0, 255, 0)
+sense.set_pixel(7, 0, 0, 0, 255)
+sense.set_pixel(7, 7, 255, 0, 255)
 
 while True:
-    dir = sense.get_compass()
-    dir_inverted = 360 - dir  # So LED appears to follow North
-    led_index = int(led_degree_ratio * dir_inverted)
-    offset = led_loop[led_index]
-
-    y = offset // 8  # row
-    x = offset % 8  # column
-
-    if x != prev_x or y != prev_y:
-        sense.set_pixel(prev_x, prev_y, 0, 0, 0)
-
-    sense.set_pixel(x, y, 0, 0, 255)
-
-    prev_x = x
-    prev_y = y
+    for r in [0, 90, 180, 270]:
+        sense.set_rotation(r)
+        time.sleep(0.3)
