c7f9b8f5ec3ecfc190ca0e1bfb0d2b383d69e942
Vitus Lehner <student@vitus-lehner.de>
2017-07-04 17:12:16 +0200

    Enhanced SpringApplication starter

--- src/main/java/org/sociotech/urbanlifeplus/microinforadiator/MicroInfoRadiatorApp.java
+++ src/main/java/org/sociotech/urbanlifeplus/microinforadiator/MicroInfoRadiatorApp.java
@@ -2,16 +2,20 @@
  * Copyright (c) 2017. Vitus Lehner. UrbanLife+. Universität der Bundeswehr München.
  */
 
-package org.sociotech.urbanlifeplus.microinforadiator.interfaces;
+package org.sociotech.urbanlifeplus.microinforadiator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.sociotech.urbanlifeplus.microinforadiator")
 public class MicroInfoRadiatorApp {
+    ;
 
     public static void main(String[] args) {
         SpringApplication.run(MicroInfoRadiatorApp.class, args);
     }
+
 
 }
