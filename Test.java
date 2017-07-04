68ed3b177b41b036cf0f0b5aa84bb6da91a73bdc
Vitus Lehner <student@vitus-lehner.de>
2017-07-05 00:34:45 +0200

    Docker resin test

--- Test.java
+++ Test.java
@@ -7,6 +7,7 @@ package test;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.util.Enumeration;
 
 public class Test {
     public static void main(String[] args) throws SocketException {
