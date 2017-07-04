7448a58eed428f024fb7227d3bd1c693dd15c186
Vitus Lehner <student@vitus-lehner.de>
2017-07-04 17:08:37 +0200

    Added RPi Sense HAT python script

---
+++ interface/src/main/resources/scripts/rpi-sensehat.py
@@ -0,0 +1,35 @@
+import sys
+
+from sense_hat import SenseHat
+from time import sleep
+
+sense = SenseHat()
+
+colors = sys.argv
+
+if len(colors) == 0:
+    print 'No colors given. Clearing HAT and exiting.'
+    sense.clear()
+    sys.exit()
+
+
+def getColorTuple(code):
+    switcher = {
+        "RED": (255, 0, 0),
+        "GREEN": (0, 255, 0),
+        "BLUE": (0, 0, 255),
+        "YELLOW": (255, 255, 0),
+        "PINK": (255, 0, 255),
+        "RED": (255, 128, 0),
+    }
+    return switcher.get(code, (0, 0, 0))
+
+
+i = 0
+while True:
+    if i == len(colors):
+        i = 0
+    c = colors[i]
+    print 'Showing color ', c, '.'
+    sense.show_letter('V', (255, 255, 255), getColorTuple(c))
+    sleep(2)
