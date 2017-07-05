7c6598bdeb13ee3c494cfa66e1e289c4812dabfc
Vitus Lehner <student@vitus-lehner.de>
2017-07-05 12:41:19 +0200

    Added REST DTO MirDeviceStatus

--- web/src/main/java/org/sociotech/urbanlifeplus/microinforadiator/web/dto/MirDeviceStatus.java
+++ web/src/main/java/org/sociotech/urbanlifeplus/microinforadiator/web/dto/MirDeviceStatus.java
@@ -1,7 +1,31 @@
+/*
+ * Copyright (c) 2017. Vitus Lehner. UrbanLife+. Universität der Bundeswehr München.
+ */
+
 package org.sociotech.urbanlifeplus.microinforadiator.web.dto;
+
+import org.sociotech.urbanlifeplus.microinforadiator.model.User;
+
+import java.util.List;
 
 /**
  * @author vituslehner 05.07.17
  */
-public class MirDeviceStatus {
+public class MirDeviceStatus extends Status {
+
+    private final String id;
+
+    private final List<User> currentUsers;
+
+    public MirDeviceStatus(boolean success, String message, String id, List<User> currentUsers) {
+        super(success, message);
+        this.id = id;
+        this.currentUsers = currentUsers;
+    }
+
+    public MirDeviceStatus(String message, String id, List<User> currentUsers) {
+        super(message);
+        this.id = id;
+        this.currentUsers = currentUsers;
+    }
 }
