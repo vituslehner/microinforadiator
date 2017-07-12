2354b1497778bbb18c10fa7c2f334dda0acb7664
Vitus Lehner <student@vitus-lehner.de>
2017-07-12 20:01:22 +0200

    Added reactor logger for debugging purposes

---
+++ core/reactor/src/main/java/org/sociotech/urbanlifeplus/microinforadiator/reactor/ReactorLogger.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2017. Vitus Lehner. UrbanLife+. Universität der Bundeswehr München.
+ */
+
+package org.sociotech.urbanlifeplus.microinforadiator.reactor;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author vituslehner 12.07.17
+ */
+@Service
+public class ReactorLogger {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ReactorLogger.class);
+
+    private final EventBus reactorEventBus;
+
+    @Autowired
+    public ReactorLogger(@Qualifier("reactorEventBus") EventBus reactorEventBus) {
+        this.reactorEventBus = reactorEventBus;
+        this.reactorEventBus.register(this);
+    }
+
+    @Subscribe()
+    public void log(Object event) {
+        LOGGER.info("REACTOR EVENT: {}", event);
+    }
+}
